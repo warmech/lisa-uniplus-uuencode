0f06d827213fcd0e44ec00b9baa33816d5c1dfa9
warmech <will_tooker@mac.com>
2024-06-04 16:20:15 -0500

    Source release

---
+++ src/uudecode.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+
+#define DEC(c) (((c) == '`' ? ' ' : (c)) - ' ' & 077)
+
+void decode(in, out)
+FILE *in;
+FILE *out;
+{
+    char buf[80]; /* Input line buffer */
+    unsigned char decoded[45]; /* Decoded data buffer */
+    int i, n;
+
+	/* Read each line of input file */
+    while (fgets(buf, sizeof(buf), in)) {
+		/* Skip lines starting with "begin" or "end" */
+        if (strncmp(buf, "begin", 5) == 0 || strncmp(buf, "end", 3) == 0)
+            continue;
+
+		/* Determine number of bytes to decode from first character of line */
+        n = DEC(buf[0]);
+        if (n <= 0)
+            continue;
+
+		/* Decode each group of four characters into three bytes */
+        for (i = 0; i < n; i += 3) {
+            unsigned char c1 = DEC(buf[1 + i * 4 / 3]);
+            unsigned char c2 = DEC(buf[2 + i * 4 / 3]);
+            unsigned char c3 = DEC(buf[3 + i * 4 / 3]);
+            unsigned char c4 = DEC(buf[4 + i * 4 / 3]);
+
+			/* Write decoded bytes to buffer */
+            if (i < n) decoded[i] = (c1 << 2) | (c2 >> 4);
+            if (i + 1 < n) decoded[i + 1] = (c2 << 4) | (c3 >> 2);
+            if (i + 2 < n) decoded[i + 2] = (c3 << 6) | c4;
+        }
+
+		/* Write decoded characters to output file */
+        fwrite(decoded, 1, n, out);
+    }
+}
+
+int main(argc, argv)
+int argc;
+char *argv[];
+{
+    FILE *infile, *outfile;
+
+	/* Check for correct number of arguments */
+    if (argc != 3) {
+        printf("Usage: %s inputfile outputfile\n", argv[0]);
+        return 1;
+    }
+
+	/* Open input file in text mode and output file in binary mode */
+    infile = fopen(argv[1], "r");
+    outfile = fopen(argv[2], "wb");
+
+	/* Check if files opened successfully */
+    if (!infile || !outfile) {
+        printf("Error opening files.\n");
+        return 1;
+    }
+
+	/* Decode file */
+    decode(infile, outfile);
+
+	/* Close files */
+    fclose(infile);
+    fclose(outfile);
+
+    return 0;
+}
---
+++ src/uuencode.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+
+#define ENC(c) (((c) & 077) + ' ')
+#define ENC_SPACE(c) ((c) == ' ' ? '`' : (c))
+
+void encode(in, out, filename)
+FILE *in;
+FILE *out;
+char *filename;
+{
+    int i, n;
+    unsigned char buf[45];
+
+	/* Write header line */
+    fprintf(out, "begin 644 %s\n", filename);
+
+	/* Read input file in chunks of 45 bytes and encode each chunk  */
+    while ((n = fread(buf, 1, 45, in)) > 0) {
+        putc(ENC_SPACE(ENC(n)), out);
+
+		/* Encode each three byte chunk */
+        for (i = 0; i < n; i += 3) {
+            unsigned char c1 = buf[i];
+            unsigned char c2 = (i + 1 < n) ? buf[i + 1] : 0;
+            unsigned char c3 = (i + 2 < n) ? buf[i + 2] : 0;
+
+			/* Write encoded characters to output file */
+            putc(ENC_SPACE(ENC(c1 >> 2)), out);
+            putc(ENC_SPACE(ENC(((c1 << 4) & 060) | ((c2 >> 4) & 017))), out);
+            putc(ENC_SPACE(ENC(((c2 << 2) & 074) | ((c3 >> 6) & 03))), out);
+            putc(ENC_SPACE(ENC(c3 & 077)), out);
+        }
+        putc('\n', out);
+    }
+
+	/* Write footer line */
+    fprintf(out, "`\nend\n");
+}
+
+int main(argc, argv)
+int argc;
+char *argv[];
+{
+    FILE *infile, *outfile;
+
+	/* Check for correct number of arguments */
+    if (argc != 3) {
+        printf("Usage: %s inputfile outputfile\n", argv[0]);
+        return 1;
+    }
+
+    infile = fopen(argv[1], "rb");
+    outfile = fopen(argv[2], "w");
+
+	/* Check if files opened successfully */
+    if (!infile || !outfile) {
+        printf("Error opening files.\n");
+        return 1;
+    }
+
+	/* Encode file */
+    encode(infile, outfile, argv[1]);
+
+	/* Close files */
+    fclose(infile);
+    fclose(outfile);
+
+    return 0;
+}
