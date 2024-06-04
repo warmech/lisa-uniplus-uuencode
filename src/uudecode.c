b8c600bc97b70819ea565098b5c03d8c381881e1
warmech <will_tooker@mac.com>
2024-06-04 17:17:05 -0500

    Comment formatting cleanup

--- src/uudecode.c
+++ src/uudecode.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+/* Macro for decoding */
 #define DEC(c) (((c) == '`' ? ' ' : (c)) - ' ' & 077)
 
 void decode(in, out)
@@ -11,31 +12,31 @@ FILE *out;
     unsigned char decoded[45]; /* Decoded data buffer */
     int i, n;
 
-	/* Read each line of input file */
+    /* Read each line of input file */
     while (fgets(buf, sizeof(buf), in)) {
-		/* Skip lines starting with "begin" or "end" */
+        /* Skip lines starting with "begin" or "end" */
         if (strncmp(buf, "begin", 5) == 0 || strncmp(buf, "end", 3) == 0)
             continue;
 
-		/* Determine number of bytes to decode from first character of line */
+        /* Determine number of bytes to decode from first character of line */
         n = DEC(buf[0]);
         if (n <= 0)
             continue;
 
-		/* Decode each group of four characters into three bytes */
+        /* Decode each group of four characters into three bytes */
         for (i = 0; i < n; i += 3) {
             unsigned char c1 = DEC(buf[1 + i * 4 / 3]);
             unsigned char c2 = DEC(buf[2 + i * 4 / 3]);
             unsigned char c3 = DEC(buf[3 + i * 4 / 3]);
             unsigned char c4 = DEC(buf[4 + i * 4 / 3]);
 
-			/* Write decoded bytes to buffer */
+            /* Write decoded bytes to buffer */
             if (i < n) decoded[i] = (c1 << 2) | (c2 >> 4);
             if (i + 1 < n) decoded[i + 1] = (c2 << 4) | (c3 >> 2);
             if (i + 2 < n) decoded[i + 2] = (c3 << 6) | c4;
         }
 
-		/* Write decoded characters to output file */
+        /* Write decoded characters to output file */
         fwrite(decoded, 1, n, out);
     }
 }
@@ -46,26 +47,26 @@ char *argv[];
 {
     FILE *infile, *outfile;
 
-	/* Check for correct number of arguments */
+    /* Check for correct number of arguments */
     if (argc != 3) {
         printf("Usage: %s inputfile outputfile\n", argv[0]);
         return 1;
     }
 
-	/* Open input file in text mode and output file in binary mode */
+    /* Open input file in text mode and output file in binary mode */
     infile = fopen(argv[1], "r");
     outfile = fopen(argv[2], "wb");
 
-	/* Check if files opened successfully */
+    /* Check if files opened successfully */
     if (!infile || !outfile) {
         printf("Error opening files.\n");
         return 1;
     }
 
-	/* Decode file */
+    /* Decode file */
     decode(infile, outfile);
 
-	/* Close files */
+    /* Close files */
     fclose(infile);
     fclose(outfile);
 
--- src/uuencode.c
+++ src/uuencode.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+/* Macros for encoding and replacing SPACE with the GRAVE character */
 #define ENC(c) (((c) & 077) + ' ')
 #define ENC_SPACE(c) ((c) == ' ' ? '`' : (c))
 
@@ -12,20 +13,20 @@ char *filename;
     int i, n;
     unsigned char buf[45];
 
-	/* Write header line */
+    /* Write header line */
     fprintf(out, "begin 644 %s\n", filename);
 
-	/* Read input file in chunks of 45 bytes and encode each chunk  */
+    /* Read input file in chunks of 45 bytes and encode each chunk  */
     while ((n = fread(buf, 1, 45, in)) > 0) {
         putc(ENC_SPACE(ENC(n)), out);
 
-		/* Encode each three byte chunk */
+        /* Encode each three byte chunk */
         for (i = 0; i < n; i += 3) {
             unsigned char c1 = buf[i];
             unsigned char c2 = (i + 1 < n) ? buf[i + 1] : 0;
             unsigned char c3 = (i + 2 < n) ? buf[i + 2] : 0;
 
-			/* Write encoded characters to output file */
+            /* Write encoded characters to output file */
             putc(ENC_SPACE(ENC(c1 >> 2)), out);
             putc(ENC_SPACE(ENC(((c1 << 4) & 060) | ((c2 >> 4) & 017))), out);
             putc(ENC_SPACE(ENC(((c2 << 2) & 074) | ((c3 >> 6) & 03))), out);
@@ -34,7 +35,7 @@ char *filename;
         putc('\n', out);
     }
 
-	/* Write footer line */
+    /* Write footer line */
     fprintf(out, "`\nend\n");
 }
 
@@ -44,7 +45,7 @@ char *argv[];
 {
     FILE *infile, *outfile;
 
-	/* Check for correct number of arguments */
+    /* Check for correct number of arguments */
     if (argc != 3) {
         printf("Usage: %s inputfile outputfile\n", argv[0]);
         return 1;
@@ -53,16 +54,16 @@ char *argv[];
     infile = fopen(argv[1], "rb");
     outfile = fopen(argv[2], "w");
 
-	/* Check if files opened successfully */
+    /* Check if files opened successfully */
     if (!infile || !outfile) {
         printf("Error opening files.\n");
         return 1;
     }
 
-	/* Encode file */
+    /* Encode file */
     encode(infile, outfile, argv[1]);
 
-	/* Close files */
+    /* Close files */
     fclose(infile);
     fclose(outfile);
 
